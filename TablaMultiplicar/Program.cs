2636b8256298e44f8dd821b765077ee902145d29
User <User@DESKTOP-MKHOC4U>
2022-02-23 21:47:28 -0500

    Agregar archivos de proyecto.

---
+++ TablaMultiplicar/Program.cs
@@ -0,0 +1,22 @@
+﻿int numTabla,inicioTabla, finTabla, producto;
+
+Console.WriteLine("Ingrese el número de la tabla que desea");
+numTabla = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el multiplicador con el cual quiere que empiece la tabla");
+inicioTabla = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese el multiplicador con el cual quiere que termine la tabla");
+finTabla = int.Parse(Console.ReadLine());
+
+for (int i = inicioTabla; i <= finTabla; i ++) 
+{
+    
+    producto = numTabla * i;
+    Console.WriteLine (numTabla+"*"+i+"="+producto);
+
+}
+
+
+
+
